778c7ff5230a48a3db42f1f86ed2660691b4e09b
Terrell Russell <terrellrussell@gmail.com>
2018-02-24 20:00:04 -0500

    fix testfile creation

--- packaging/test_plugin_tiered_storage.py
+++ packaging/test_plugin_tiered_storage.py
@@ -222,6 +222,7 @@ class TestStorageTieringPluginMultiGroup(ResourceBase, unittest.TestCase):
                 filename = 'test_put_file'
 
                 filenameg2 = 'test_put_fileg2'
+                lib.create_local_testfile(filenameg2)
 
                 admin_session.assert_icommand('iput -R rnd0 ' + filename)
                 admin_session.assert_icommand('imeta ls -d ' + filename, 'STDOUT_SINGLELINE', filename)
