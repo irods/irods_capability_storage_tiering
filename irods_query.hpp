8b705ef99c93e34397e7200f982e359627e253a6
Jason Coposky <jasonc@renci.org>
2018-03-24 23:54:28 -0400

    [#19] query strategy in support of general and specific queries

--- irods_query.hpp
+++ irods_query.hpp
@@ -123,26 +123,20 @@ namespace irods {
             }
 
             value_type capture_results(int _row_idx) {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d - row idx %d attriCnt %d", __FUNCTION__, __LINE__, _row_idx, gen_output_->attriCnt);
                 value_type res;
                 for(int attr_idx = 0; attr_idx < gen_output_->attriCnt; ++attr_idx) {
                     uint32_t offset = gen_output_->sqlResult[attr_idx].len * _row_idx;
                     std::string str{&gen_output_->sqlResult[attr_idx].value[offset]};
-rodsLog(LOG_NOTICE, "XXXX - %s:%d - attr idx %d, str [%s]", __FUNCTION__, __LINE__, attr_idx, str.c_str());
                     res.push_back(str);
                 }
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 return res;
             }
 
             bool results_valid() {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 if(gen_output_) {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d - row cnt %d", __FUNCTION__, __LINE__, gen_output_->rowCnt);
                     return (gen_output_->rowCnt > 0);
                 }
                 else {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     return false;
                 }
             }
@@ -171,20 +165,15 @@ rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
             }
 
             int fetch_page() {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 if(gen_output_) {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     freeGenQueryOut(&gen_output_);
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     gen_input_.continueInx = gen_output_->continueInx;
                 }
 
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 int ret = query_helper::gen_query_fcn(
                            comm_,
                            &gen_input_,
                            &gen_output_);
-rodsLog(LOG_NOTICE, "XXXX - %s:%d - row cnt %d", __FUNCTION__, __LINE__, gen_output_->rowCnt);
                 return ret;
             } // fetch_page
 
@@ -194,28 +183,66 @@ rodsLog(LOG_NOTICE, "XXXX - %s:%d - row cnt %d", __FUNCTION__, __LINE__, gen_out
                 const std::string&       _query_string) :
                 query_impl_base(_comm, _query_string) {
 
-rodsLog(LOG_NOTICE, "XXXX - %s:%d - qstr [%s] max rows [%d]", __FUNCTION__, __LINE__, _query_string.c_str(),_max_rows);
                 memset(&gen_input_, 0, sizeof(gen_input_));
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 gen_input_.maxRows = _max_rows;
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 const int fill_err = fillGenQueryInpFromStrCond(
                                          const_cast<char*>(_query_string.c_str()),
                                          &gen_input_);
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 if(fill_err < 0) {
                     THROW(
                         fill_err,
                         boost::format("query fill failed for [%s]") %
                         _query_string);
                 }
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
             } // ctor
 
             private:
             genQueryInp_t gen_input_; 
-        };
+        }; // class gen_query_impl
 
+        class spec_query_impl : public query_impl_base {
+            public:
+            virtual ~spec_query_impl() {
+            }
+
+            int fetch_page() {
+                if(gen_output_) {
+                    freeGenQueryOut(&gen_output_);
+                    spec_input_.continueInx = gen_output_->continueInx;
+                }
+
+                int ret = query_helper::spec_query_fcn(
+                           comm_,
+                           &spec_input_,
+                           &gen_output_);
+                return ret;
+            } // fetch_page
+
+            spec_query_impl(
+                query_helper::comm_type* _comm,
+                int                      _max_rows,
+                const std::string&       _query_string) :
+                query_impl_base(_comm, _query_string) {
+
+                memset(&spec_input_, 0, sizeof(spec_input_));
+                spec_input_.maxRows = _max_rows;
+                spec_input_.sql = const_cast<char*>(_query_string.c_str());
+
+                int spec_err = query_helper::spec_query_fcn(
+                                   _comm,
+                                   &spec_input_,
+                                   &gen_output_);
+                if(spec_err < 0) {
+                    THROW(
+                        spec_err,
+                        boost::format("query fill failed for [%s]") %
+                        _query_string);
+                }
+            } // ctor
+
+            private:
+            specificQueryInp_t spec_input_; 
+        }; // class spec_query_impl
 
         class iterator: public std::iterator<
             std::forward_iterator_tag,// iterator_category
@@ -231,7 +258,7 @@ rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 genQueryOut_t* gen_output_; 
                 bool end_iteration_state_;
 
-                query_impl_base* query_impl_;
+                std::shared_ptr<query_impl_base> query_impl_;
 
                 public:
                 explicit iterator () :
@@ -245,12 +272,12 @@ rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 }
 
                 explicit iterator(
-                    query_impl_base* _qimp) :
+                    std::shared_ptr<query_impl_base> _qimp) :
                     query_impl_(_qimp),
                     row_idx_{},
                     end_iteration_state_{false} {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 }
+
                 explicit iterator(
                     query_helper::comm_type* _comm,
                     const std::string&       _query_string,
@@ -267,109 +294,48 @@ rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 } // ctor
 
                 iterator operator++() {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     advance_query();
                     return *this;
                 }
                 
                 iterator operator++(int) {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     iterator ret = *this;
                     ++(*this);
                     return ret;
                 }
 
                 bool operator==(const iterator& _rhs) const {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     if(end_iteration_state_ && _rhs.end_iteration_state_) {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                         return true;
                     }
-rodsLog(LOG_NOTICE, "XXXX - %s:%d ql [%s] qr [%s]", __FUNCTION__, __LINE__, query_impl_->query_string().c_str(), _rhs.query_string_.c_str());
                     //return (query_string_ == _rhs.query_string_);
                     return (query_impl_->query_string() == _rhs.query_string_);
                 }
                
                 bool operator!=(const iterator& _rhs) const {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     bool val = !(*this == _rhs);
                     return val;
                 }
 
                 value_type operator*() {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     return capture_results();
                 }
-#if 0
+
                 void advance_query() {
                     row_idx_++;
 
-                    // not at end of page, have valid results
-                    if(row_idx_ < gen_output_->rowCnt) {
-                        return;
-                    }
-
-                    // finished page, and out of pages
-                    if(gen_output_->continueInx <= 0) {
-                        end_iteration_state_ = true;
-                        return;
-                    }
-
-                    // finished page, attempt to fetch another
-                    row_idx_ = 0; // reset row counter
-                    gen_input_->continueInx = gen_output_->continueInx;
-                    freeGenQueryOut(&gen_output_);
-
-                    const int query_err = query_helper::gen_query_fcn(comm_, gen_input_, &gen_output_);
-                    if(query_err < 0) {
-                        if(CAT_NO_ROWS_FOUND != query_err) {
-                            THROW(
-                                query_err,
-                                boost::format("gen query failed for [%s] on idx %d") %
-                                query_string_ %
-                                gen_input_->continueInx);
-                        }
-
-                       end_iteration_state_ = true;
-                
-                    } // if
-
-                } // advance_query
-
-                value_type capture_results() {
-                    value_type res;
-                    for(int attr_idx = 0; attr_idx < gen_output_->attriCnt; ++attr_idx) {
-                        uint32_t offset = gen_output_->sqlResult[attr_idx].len * row_idx_;
-                        std::string str{&gen_output_->sqlResult[attr_idx].value[offset]};
-                        res.push_back(str);
-                    }
-                    return res;
-                }
-#else
-                void advance_query() {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
-                    row_idx_++;
-
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     if(query_impl_->page_in_flight(row_idx_)) {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                         return;
                     }
 
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     if(query_impl_->query_complete()) {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                         end_iteration_state_ = true;
                         return;
                     }
 
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     const int query_err = query_impl_->fetch_page();
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     if(query_err < 0) {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                         if(CAT_NO_ROWS_FOUND != query_err) {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                             THROW(
                                 query_err,
                                 boost::format("gen query failed for [%s] on idx %d") %
@@ -380,86 +346,39 @@ rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                        end_iteration_state_ = true;
                 
                     } // if
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
 
                 } // advance_query 
 
                 value_type capture_results() {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d - row_irx_ %d", __FUNCTION__, __LINE__, row_idx_);
                     return query_impl_->capture_results(row_idx_);
                 }
-#endif
+
          }; // class iterator
-#if 0
-        explicit query(
-            query_helper::comm_type*   _comm,
-            const std::string& _query_string,
-            uintmax_t _max_rows = MAX_SQL_ROWS,
-            query_type _query_type = GENERAL) :
-            gen_output_{},
-            query_type_{_query_type} {
-
-            memset(&gen_input_, 0, sizeof(gen_input_));
-            gen_input_.maxRows = _max_rows;
-            const int fill_err = fillGenQueryInpFromStrCond(
-                                     const_cast<char*>(_query_string.c_str()),
-                                     &gen_input_);
-            if(fill_err < 0) {
-                THROW(
-                    fill_err,
-                    boost::format("query fill failed for [%s]") %
-                    _query_string);
-            }
-
-            const int query_err = query_helper::gen_query_fcn(
-                                      _comm,
-                                      &gen_input_,
-                                      &gen_output_);
-            if(query_err < 0) {
-                if(CAT_NO_ROWS_FOUND == query_err) {
-                    iter_ = std::make_unique<iterator>();
-                }
-                else {
-                    THROW(
-                        query_err,
-                        boost::format("gen query failed for [%s]") %
-                        _query_string);
-                }
-            }
-
-            if(gen_output_ && (gen_output_->rowCnt > 0)) {
-                iter_ = std::make_unique<iterator>(_comm, _query_string, _max_rows, &gen_input_, gen_output_);
-            }
-            else {
-                iter_ = std::make_unique<iterator>();
-            }
-        } // query ctor
-#else
+        
         explicit query(
             query_helper::comm_type* _comm,
             const std::string&       _query_string,
             uintmax_t                _max_rows   = MAX_SQL_ROWS,
-            query_type               _query_type = GENERAL) :
-            query_type_{_query_type} {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d - qstr [%s] max rows [%d]", __FUNCTION__, __LINE__, _query_string.c_str(),_max_rows);
-                if(query_type_ == GENERAL) {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
-                    query_impl_ = new gen_query_impl(
-                                          _comm,
-                                          _max_rows,
-                                          _query_string);
+            query_type               _query_type = GENERAL) {
+                if(_query_type == GENERAL) {
+                    query_impl_ = std::make_shared<gen_query_impl>(
+                                      _comm,
+                                      _max_rows,
+                                      _query_string);
+                }
+                else if(_query_type == SPECIFIC) {
+                    query_impl_ = std::make_shared<spec_query_impl>(
+                                      _comm,
+                                      _max_rows,
+                                      _query_string);
                 }
 
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 const int fetch_err = query_impl_->fetch_page(); 
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 if(fetch_err < 0) {
                     if(CAT_NO_ROWS_FOUND == fetch_err) {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                         iter_ = std::make_unique<iterator>();
                     }
                     else {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                         THROW(
                             fetch_err,
                             boost::format("query failed for [%s] type [%d]") %
@@ -468,52 +387,33 @@ rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     }
                 }
 
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 if(query_impl_->results_valid()) {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     iter_ = std::make_unique<iterator>(query_impl_);
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                 }
                 else {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
                     iter_ = std::make_unique<iterator>();
                 }
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
         } // ctor
-#endif
+
         ~query() {
-            //clearGenQueryInp(&gen_input_);
-            //freeGenQueryOut(&gen_output_); 
         }
 
         iterator   begin() {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
             return *iter_;
         }
         iterator   end()   {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
             return iterator();
         }
         value_type front() {
-rodsLog(LOG_NOTICE, "XXXX - %s:%d", __FUNCTION__, __LINE__);
             return (*(*iter_));
         }
 
         size_t size()  {
-#if 0
-            if(!gen_output_) {return 0;}
-            return gen_output_->rowCnt;
-#else
             return query_impl_->size();
-#endif
         }
     private:
-        genQueryInp_t             gen_input_; 
-        genQueryOut_t*            gen_output_; 
-        const query_type          query_type_;
-        std::unique_ptr<iterator> iter_;
-
-        query_impl_base*          query_impl_;
+        std::unique_ptr<iterator>        iter_;
+        std::shared_ptr<query_impl_base> query_impl_;
     }; // class query
 } // namespace irods
 
