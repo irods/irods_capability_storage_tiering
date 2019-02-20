c563a864322d87a8c044ad9aa0bb7565fc39f92e
Jason Coposky <jasonc@renci.org>
2019-02-20 13:24:18 -0500

    [#20] externalize policy invocations
    The storage tiering rule engine plugin now externalizes
    the calls to the following policy enforcement points which
    allows for the possibility of custom implementation.
    
    irods_policy_apply_access_time
    irods_policy_data_movement
    irods_policy_data_replication
    irods_policy_data_verification

--- example_tiering_invocation.r
+++ example_tiering_invocation.r
@@ -1,7 +1,7 @@
 {
-   "rule-engine-instance-name": "irods_rule_engine_plugin-storage_tiering-instance",
-   "rule-engine-operation": "apply_storage_tiering_policy",
-   "delay-parameters": "<PLUSET>1s</PLUSET><EF>1h DOUBLE UNTIL SUCCESS OR 6 TIMES</EF>",
+   "rule-engine-instance-name": "irods-rule-engine-plugin-storage-tiering-instance",
+   "rule-engine-operation": "irods_policy_schedule_storage_tiering",
+   "delay-parameters": "<INST_NAME>irods-rule-engine-plugin-storage-tiering-instance</INST_NAME><PLUSET>1s</PLUSET><EF>1h DOUBLE UNTIL SUCCESS OR 6 TIMES</EF>",
    "storage-tier-groups": [
        "example_group_g2",
        "example_group"
