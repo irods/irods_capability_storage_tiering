ae17e7371c9cf2655b7129209f6d6ed0fa9dfe0a
Jason Coposky <jasonc@renci.org>
2019-03-04 15:19:46 -0500

    [#20] refactor cmake

--- data_verification.cmake
+++ data_verification.cmake
@@ -1,10 +1,10 @@
-set(POLICY_NAME "data-verification")
+set(POLICY_NAME "data_verification")
 
-set(IRODS_PACKAGE_COMPONENT_POLICY_NAME "${POLICY_NAME}")
+set(IRODS_PACKAGE_COMPONENT_POLICY_NAME "data-verification")
 #set -${IRODS_PLUGIN_VERSION}")
 set(TOUPPER IRODS_PACKAGE_COMPONENT_POLICY_NAME_UPPERCASE ${IRODS_PACKAGE_COMPONENT_POLICY_NAME})
 
-set(TARGET_NAME "irods-rule-engine-plugin-${POLICY_NAME}")
+set(TARGET_NAME "irods_rule_engine_plugin-${POLICY_NAME}")
 
 set(
   IRODS_PLUGIN_POLICY_COMPILE_DEFINITIONS
@@ -17,11 +17,10 @@ set(
   irods_server
   )
 
-string(REPLACE "-" "_" POLICY_NAME_US ${POLICY_NAME})
 add_library(
     ${TARGET_NAME}
     MODULE
-    ${CMAKE_SOURCE_DIR}/libirods_rule_engine_plugin-${POLICY_NAME_US}.cpp
+    ${CMAKE_SOURCE_DIR}/lib${TARGET_NAME}.cpp
     ${CMAKE_SOURCE_DIR}/storage_tiering_configuration.cpp
     ${CMAKE_SOURCE_DIR}/storage_tiering_utilities.cpp
     ${CMAKE_SOURCE_DIR}/data_verification_utilities.cpp
