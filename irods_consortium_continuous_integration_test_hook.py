ab6975ebc1f009b59e6610b3d5f8251e9205de1e
Jaspreet Gill <jessi.k.gill@gmail.com>
2019-10-10 15:42:37 -0400

    changes made to run the tests in docker container

--- irods_consortium_continuous_integration_test_hook.py
+++ irods_consortium_continuous_integration_test_hook.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-import optparse
+import argparse
 import os
 import shutil
 import glob
@@ -8,15 +8,16 @@ import time
 import irods_python_ci_utilities
 
 def main():
-    parser = optparse.OptionParser()
-    parser.add_option('--output_root_directory')
-    parser.add_option('--built_packages_root_directory')
-    parser.add_option('--mungefs_packages_directory')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output_root_directory', type=str, required=True)
+    parser.add_argument('--built_packages_root_directory', type=str, required=True)
+    parser.add_argument('--munge_path', type=str, default=None, help='munge externals path')
+    parser.add_argument('--test_unified_storage_tiering', action='store_true', default=False)
 
-    options, _ = parser.parse_args()
+    args = parser.parse_args()
 
-    output_root_directory = options.output_root_directory
-    built_packages_root_directory = options.built_packages_root_directory
+    output_root_directory = args.output_root_directory
+    built_packages_root_directory = args.built_packages_root_directory
     package_suffix = irods_python_ci_utilities.get_package_suffix()
     os_specific_directory = irods_python_ci_utilities.append_os_specific_directory(built_packages_root_directory)
     irods_python_ci_utilities.install_os_packages_from_files(glob.glob(os.path.join(os_specific_directory, 'irods-rule-engine-plugin-apply-access-time*.{0}'.format(package_suffix))))
@@ -32,7 +33,14 @@ def main():
 
     try:
         test_output_file = 'log/test_output.log'
-        irods_python_ci_utilities.subprocess_get_output(['sudo', 'su', '-', 'irods', '-c', 'python2 scripts/run_tests.py --xml_output --run_s test_plugin_storage_tiering 2>&1 | tee {0}; exit $PIPESTATUS'.format(test_output_file)], check_rc=True)
+        test_name = 'test_plugin_storage_tiering'
+        if args.test_unified_storage_tiering:
+            test_name = 'test_plugin_unified_storage_tiering'
+
+        if args.munge_path is not None or args.munge_path != '':
+            irods_python_ci_utilities.subprocess_get_output(['sudo', 'su', '-', 'irods', '-c', 'cd scripts; {0}; python2 run_tests.py --xml_output --run_s {1} 2>&1 | tee {2}; exit $PIPESTATUS'.format(args.munge_path, test_name, test_output_file)], check_rc=True)
+        else:
+            irods_python_ci_utilities.subprocess_get_output(['sudo', 'su', '-', 'irods', '-c', 'python2 scripts/run_tests.py --xml_output --run_s {0} 2>&1 | tee {1}; exit $PIPESTATUS'.format(test_name, test_output_file)], check_rc=True)
     finally:
         if output_root_directory:
             irods_python_ci_utilities.gather_files_satisfying_predicate('/var/lib/irods/log', output_root_directory, lambda x: True)
