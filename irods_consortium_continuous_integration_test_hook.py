db18f4b53a0910236cf76a514eeabb38ac81cb21
Jaspreet Gill <jessi.k.gill@gmail.com>
2020-04-04 15:30:38 -0400

    [#138] removed the reference to old packages and test from the hook

--- irods_consortium_continuous_integration_test_hook.py
+++ irods_consortium_continuous_integration_test_hook.py
@@ -20,18 +20,10 @@ def main():
     built_packages_root_directory = args.built_packages_root_directory
     package_suffix = irods_python_ci_utilities.get_package_suffix()
     os_specific_directory = irods_python_ci_utilities.append_os_specific_directory(built_packages_root_directory)
-    irods_python_ci_utilities.install_os_packages_from_files(glob.glob(os.path.join(os_specific_directory, 'irods-rule-engine-plugin-apply-access-time*.{0}'.format(package_suffix))))
-    irods_python_ci_utilities.install_os_packages_from_files(glob.glob(os.path.join(os_specific_directory, 'irods-rule-engine-plugin-data-movement*.{0}'.format(package_suffix))))
-    irods_python_ci_utilities.install_os_packages_from_files(glob.glob(os.path.join(os_specific_directory, 'irods-rule-engine-plugin-data-replication*.{0}'.format(package_suffix))))
-    irods_python_ci_utilities.install_os_packages_from_files(glob.glob(os.path.join(os_specific_directory, 'irods-rule-engine-plugin-data-verification*.{0}'.format(package_suffix))))
-    irods_python_ci_utilities.install_os_packages_from_files(glob.glob(os.path.join(os_specific_directory, 'irods-rule-engine-plugin-storage-tiering*.{0}'.format(package_suffix))))
-    irods_python_ci_utilities.install_os_packages_from_files(glob.glob(os.path.join(os_specific_directory, 'irods-rule-engine-plugin-unified-storage-tiering*.{0}'.format(package_suffix))))
+    irods_python_ci_utilities.subprocess_get_output(['sudo', '-EH', 'pip', 'install', 'unittest-xml-reporting==1.14.0'])
+    irods_python_ci_utilities.install_os_packages_from_files(glob.glob(os.path.join(os_specific_directory, 'irods-rule-engine-plugin-unified-storage-tiering*')))
     
-    test_name = None
-    if args.test_unified_storage_tiering == 'True':
-        test_name = 'test_plugin_unified_storage_tiering'
-    else:
-        test_name = 'test_plugin_storage_tiering'
+    test_name = 'test_plugin_unified_storage_tiering'
 
     time.sleep(10)
     irods_python_ci_utilities.subprocess_get_output(['sudo', 'chmod', 'g+rwx', '/dev/fuse'], check_rc=True)
@@ -49,7 +41,7 @@ def main():
         if output_root_directory:
             irods_python_ci_utilities.gather_files_satisfying_predicate('/var/lib/irods/log', output_root_directory, lambda x: True)
             shutil.copy('/var/lib/irods/log/test_output.log', output_root_directory)
-            #shutil.copytree('/var/lib/irods/test-reports', os.path.join(output_root_directory, 'test-reports'))
+            shutil.copytree('/var/lib/irods/test-reports', os.path.join(output_root_directory, 'test-reports'))
 
 
 if __name__ == '__main__':
