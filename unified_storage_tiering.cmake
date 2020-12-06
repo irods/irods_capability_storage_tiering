4e32bbd6ec68eb16c9c7eba17d9db871a8101e09
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2020-12-06 15:07:16 -0500

    [irods/irods#5220] cmake: use GNUInstallDirs

--- unified_storage_tiering.cmake
+++ unified_storage_tiering.cmake
@@ -58,7 +58,7 @@ install(
   TARGETS
   ${TARGET_NAME}
   LIBRARY
-  DESTINATION usr/lib/irods/plugins/rule_engines
+  DESTINATION ${IRODS_PLUGINS_DIRECTORY}/rule_engines
   COMPONENT ${IRODS_PACKAGE_COMPONENT_POLICY_NAME}
   )
 
