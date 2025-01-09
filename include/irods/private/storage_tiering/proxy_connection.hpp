41cf297d022ada9916f5ab685928b32ec83ca43f
Alan King <alanking@renci.org>
2025-01-09 10:54:43 -0500

    [#164,#189,#273] Run all storage tiering ops as admin
    This commit makes a couple of changes:
    
    1. Added proxy_connection::make_rodsadmin_connection.
    This function simply establishes a client connection as the
    local service account rodsadmin. Importantly, it also sets
    the authFlag in the created RcComm in order to appropriately
    indicate the privilege level of the client user.
    
    2. All instances of establishing a connection with the local
    server to perform actions have been replaced with connections
    as the local service account rodsadmin. This allows storage tiering
    to do its job properly without needing to worry about user
    permissions. Note: Violating queries still select the USER_NAME
    and USER_ZONE, but they are no longer used.

--- include/irods/private/storage_tiering/proxy_connection.hpp
+++ include/irods/private/storage_tiering/proxy_connection.hpp
@@ -9,29 +9,58 @@ namespace irods {
         rErrMsg_t err_msg;
         rcComm_t* conn;
 
-        auto make(const std::string clientUser = "", const std::string clientZone = "") -> rcComm_t*
+        // Makes a proxy connection where the client is specified by the username in the parameters, and the proxy user
+        // is the service account rodsadmin for the local server.
+        auto make(const std::string& clientUser, const std::string& clientZone) -> rcComm_t*
         {
             rodsEnv env{};
             _getRodsEnv(env);
 
-            conn = _rcConnect(
-                       env.rodsHost,
-                       env.rodsPort,
-                       env.rodsUserName,
-                       env.rodsZone,
-                       !clientUser.empty() ?
-                           clientUser.c_str() :
-                           env.rodsUserName,
-                       !clientZone.empty() ?
-                           clientZone.c_str() :
-                           env.rodsZone,
-                       &err_msg,
-                       0, 0);
+            // TODO(#296): Handle any errors which occur in _rcConnect or clientLogin.
+            conn = _rcConnect(env.rodsHost,
+                              env.rodsPort,
+                              env.rodsUserName,
+                              env.rodsZone,
+                              clientUser.c_str(),
+                              clientZone.c_str(),
+                              &err_msg,
+                              0,
+                              0);
 
             clientLogin(conn);
 
             return conn;
         } // make
+
+        // Makes a proxy connection where both the proxy and client users are the service account rodsadmin for the
+        // local server.
+        auto make_rodsadmin_connection() -> RcComm*
+        {
+            rodsEnv env{};
+            _getRodsEnv(env);
+
+            // TODO(#296): Handle any errors which occur in _rcConnect or clientLogin.
+            conn = _rcConnect(env.rodsHost,
+                              env.rodsPort,
+                              env.rodsUserName,
+                              env.rodsZone,
+                              env.rodsUserName,
+                              env.rodsZone,
+                              &err_msg,
+                              0,
+                              0);
+
+            clientLogin(conn);
+
+            // Set the authFlag because auth plugin does not set it and the storage tiering plugin needs to know whether
+            // the client connection is privileged. This proxy connection uses the local client environment which should
+            // be the iRODS service account, a rodsadmin. If the local client environment is not a rodsadmin, the plugin
+            // will not function properly because it uses the ADMIN_KW and the server does not allow non-rodsadmins to
+            // use the ADMIN_KW.
+            conn->clientUser.authInfo.authFlag = LOCAL_PRIV_USER_AUTH;
+
+            return conn;
+        } // make_rodsadmin_connection
 
         ~proxy_connection() { rcDisconnect(conn); }
     }; // proxy_connection
